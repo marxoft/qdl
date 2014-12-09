4c3074c201fffa1cdc2f43ff22f0f2242e17065e
Stuart Howarth <showarth@marxoft.co.uk>
2014-12-09 19:40:41 +0000

    Add Filespace plugin.

---
+++ qdl-plugins/service-plugins/filespace/filespace.cpp
@@ -0,0 +1,323 @@
+/*
+ * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "filespace.h"
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QTimer>
+#include <QRegExp>
+
+Filespace::Filespace(QObject *parent) :
+    ServicePlugin(parent),
+    m_waitTimer(new QTimer(this)),
+    m_waitTime(0),
+    m_connections(1)
+{
+    this->connect(m_waitTimer, SIGNAL(timeout()), this, SLOT(updateWaitTime()));
+}
+
+QRegExp Filespace::urlPattern() const {
+    return QRegExp("http(s|)://(www\\.|)filespace.com/\\w+", Qt::CaseInsensitive);
+}
+
+bool Filespace::urlSupported(const QUrl &url) const {
+    return this->urlPattern().indexIn(url.toString()) == 0;
+}
+
+void Filespace::login(const QString &username, const QString &password) {
+    QString data = QString("op=login&redirect=&login=%1&password=%2").arg(username).arg(password);
+    QUrl url("http://filespace.com");
+    QNetworkRequest request(url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QNetworkReply *reply = this->networkAccessManager()->post(request, data.toUtf8());
+    this->connect(reply, SIGNAL(finished()), this, SLOT(checkLogin()));
+    this->connect(this, SIGNAL(currentOperationCancelled()), reply, SLOT(deleteLater()));
+}
+
+void Filespace::checkLogin() {
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(this->sender());
+
+    if (!reply) {
+        emit error(NetworkError);
+        return;
+    }
+
+    int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+
+    switch (statusCode) {
+    case 302:
+    case 200:
+    case 201:
+        m_connections = 0;
+        emit loggedIn(true);
+        break;
+    default:
+        m_connections = 1;
+        emit loggedIn(false);
+        break;
+    }
+
+    reply->deleteLater();
+}
+
+void Filespace::checkUrl(const QUrl &url) {
+    QNetworkRequest request(url);
+    request.setRawHeader("Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6");
+    QNetworkReply *reply = this->networkAccessManager()->get(request);
+    this->connect(reply, SIGNAL(finished()), this, SLOT(checkUrlIsValid()));
+    this->connect(this, SIGNAL(currentOperationCancelled()), reply, SLOT(deleteLater()));
+}
+
+void Filespace::checkUrlIsValid() {
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(this->sender());
+
+    if (!reply) {
+        emit urlChecked(false);
+        return;
+    }
+
+    QString redirect = reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toString();
+    QRegExp re("http://\\w+\\.filespace.com:\\d+/[^'\"]+");
+
+    if ((!redirect.isEmpty()) && (re.indexIn(redirect) == -1)) {
+        this->checkUrl(QUrl(redirect));
+    }
+    else {
+        QString response(reply->readAll());
+
+        if (response.contains("File not found")) {
+            emit urlChecked(false);
+        }
+        else {
+            QString fileName = response.section("fname\" value=\"", 1, 1).section('"', 0, 0);
+
+            if (fileName.isEmpty()) {
+                emit urlChecked(false);
+            }
+            else {
+                emit urlChecked(true, reply->request().url(), this->serviceName(), fileName);
+            }
+        }
+    }
+
+    reply->deleteLater();
+}
+
+void Filespace::getDownloadRequest(const QUrl &url) {
+    emit statusChanged(Connecting);
+    m_url = url;
+    QNetworkRequest request(url);
+    request.setRawHeader("Accept-Language", "en-GB,en-US;q=0.8,en;q=0.6");
+    QNetworkReply *reply = this->networkAccessManager()->get(request);
+    this->connect(reply, SIGNAL(finished()), this, SLOT(onWebPageDownloaded()));
+    this->connect(this, SIGNAL(currentOperationCancelled()), reply, SLOT(deleteLater()));
+}
+
+void Filespace::onWebPageDownloaded() {
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(this->sender());
+
+    if (!reply) {
+        emit error(NetworkError);
+        return;
+    }
+
+    QRegExp re("http://\\w+\\.filespace.com:\\d+/[^'\"]+");
+    QString redirect = reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toString();
+
+    if (re.indexIn(redirect) == 0) {
+        QNetworkRequest request;
+        request.setUrl(QUrl(re.cap()));
+        emit downloadRequestReady(request);
+    }
+    else if (!redirect.isEmpty()) {
+        this->getDownloadRequest(QUrl(redirect));
+    }
+    else {
+        QString response(reply->readAll());
+
+        if (re.indexIn(response) >= 0) {
+            QNetworkRequest request;
+            request.setUrl(QUrl(re.cap()));
+            emit downloadRequestReady(request);
+        }
+        else if (response.contains("File Not Found")) {
+            emit error(NotFound);
+        }
+        else {
+            m_fileId = response.section("id\" value=\"", 1, 1).section('"', 0, 0);
+            m_fileName = response.section("fname\" value=\"", 1, 1).section('"', 0, 0);
+            
+            if ((m_fileId.isEmpty()) || (m_fileName.isEmpty())) {
+                emit error(UnknownError);
+            }
+            else {
+                this->getWaitTime();
+            }
+        }
+    }
+
+    reply->deleteLater();
+}
+
+void Filespace::getWaitTime() {
+    QString data = QString("op=download1&id=%1&fname=%2&method_free=Free+Download").arg(m_fileId).arg(m_fileName);
+    QNetworkRequest request(m_url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QNetworkReply *reply = this->networkAccessManager()->post(request, data.toUtf8());
+    this->connect(reply, SIGNAL(finished()), this, SLOT(checkWaitTime()));
+    this->connect(this, SIGNAL(currentOperationCancelled()), reply, SLOT(deleteLater()));
+}
+
+void Filespace::checkWaitTime() {
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(this->sender());
+
+    if (!reply) {
+        emit error(NetworkError);
+        return;
+    }
+
+    QString response(reply->readAll());
+    int mins = 0;
+    int secs = 0;
+
+    if (response.contains("You have to wait")) {
+        mins = response.section("You have to wait ", 1, 1).section(" minutes", 0, 0).toInt();
+        secs = response.section(" seconds till next download", 0, 0).section(' ', 1, 1).toInt();
+        this->startWait((mins * 60000) + (secs + 1000));
+        this->connect(this, SIGNAL(waitFinished()), this, SLOT(onWaitFinished()));
+    }
+    else if (response.contains("You can download files up to ")) {
+        emit error(TrafficExceeded);
+    }
+    else if (response.contains("premium account to download this file")) {
+        this->setErrorString(tr("Premium account required"));
+        emit error(Unauthorised);
+    }
+    else if (response.contains("reached the download limit for unregistered users")) {
+        this->startWait(600000);
+        this->connect(this, SIGNAL(waitFinished()), this, SLOT(onWaitFinished()));
+    }
+    else {
+        secs = response.section(QRegExp("Please wait <span id=\"\\w+\">"), 1, 1).section('<', 0, 0).toInt();
+        m_rand = response.section("rand\" value=\"", 1, 1).section('"', 0, 0);
+        m_captchaKey = response.section("api.solvemedia.com/papi/challenge.noscript?k=", 1, 1).section('"', 0, 0);
+
+        if ((m_rand.isEmpty()) || (m_captchaKey.isEmpty())) {
+            emit error(UnknownError);
+        }
+        else {
+            if (secs > 0) {
+                m_waitTime = secs * 1000;
+                this->startWait(m_waitTime);
+                this->connect(this, SIGNAL(waitFinished()), this, SLOT(downloadCaptcha()));
+            }
+            else {
+                emit statusChanged(CaptchaRequired);
+            }
+        }
+    }
+
+    reply->deleteLater();
+}
+
+void Filespace::downloadCaptcha() {
+    emit statusChanged(CaptchaRequired);
+    this->disconnect(this, SIGNAL(waitFinished()), this, SLOT(downloadCaptcha()));
+}
+
+void Filespace::submitCaptchaResponse(const QString &challenge, const QString &response) {
+    QString data = QString("op=download2&id=%1&fname=%2&rand=%3&method_free=Free+Download&down_direct=1&adcopy_challenge=%4&adcopy_response=%5").arg(m_fileId).arg(m_fileName).arg(m_rand).arg(challenge).arg(response);
+    QNetworkRequest request(m_url);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    QNetworkReply *reply = this->networkAccessManager()->post(request, data.toUtf8());
+    this->connect(reply, SIGNAL(finished()), this, SLOT(onCaptchaSubmitted()));
+    this->connect(this, SIGNAL(currentOperationCancelled()), reply, SLOT(deleteLater()));
+}
+
+void Filespace::onCaptchaSubmitted() {
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(this->sender());
+
+    if (!reply) {
+        emit error(NetworkError);
+        return;
+    }
+
+    QRegExp re("http://\\w+\\.filespace.com:\\d+/[^'\"]+");
+    QString redirect = reply->attribute(QNetworkRequest::RedirectionTargetAttribute).toString();
+
+    if (re.indexIn(redirect) == 0) {
+        QNetworkRequest request;
+        request.setUrl(QUrl(re.cap()));
+        emit downloadRequestReady(request);
+    }
+    else {
+        QString response(reply->readAll());
+
+        if (re.indexIn(response) >= 0) {
+            QNetworkRequest request;
+            request.setUrl(QUrl(re.cap()));
+            emit downloadRequestReady(request);
+        }
+        else if (response.contains("wrong answer")) {
+            emit error(CaptchaError);
+        }
+        else {
+            emit error(UnknownError);
+        }
+    }
+
+    reply->deleteLater();
+}
+
+void Filespace::startWait(int msecs) {
+    if (msecs > m_waitTime) {
+        emit statusChanged(LongWait);
+    }
+    else {
+        emit statusChanged(ShortWait);
+    }
+
+    emit waiting(msecs);
+    m_waitTime = msecs;
+    m_waitTimer->start(1000);
+}
+
+void Filespace::updateWaitTime() {
+    m_waitTime -= m_waitTimer->interval();
+    emit waiting(m_waitTime);
+
+    if (m_waitTime <= 0) {
+        m_waitTimer->stop();
+        emit waitFinished();
+    }
+}
+
+void Filespace::onWaitFinished() {
+    emit statusChanged(Ready);
+    this->disconnect(this, SIGNAL(waitFinished()), this, SLOT(onWaitFinished()));
+}
+
+bool Filespace::cancelCurrentOperation() {
+    m_waitTimer->stop();
+    this->disconnect(this, SIGNAL(waitFinished()), this, 0);
+    emit currentOperationCancelled();
+
+    return true;
+}
+
+Q_EXPORT_PLUGIN2(filespace, Filespace)
---
+++ qdl-plugins/service-plugins/filespace/filespace.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 Stuart Howarth <showarth@marxoft.co.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU Lesser General Public License,
+ * version 3, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef FILESPACE_H
+#define FILESPACE_H
+
+#include <QObject>
+#include <QUrl>
+#include "serviceplugin.h"
+
+class QTimer;
+
+class Filespace : public ServicePlugin
+{
+    Q_OBJECT
+    Q_INTERFACES(ServiceInterface)
+
+public:
+    explicit Filespace(QObject *parent = 0);
+    ServicePlugin* createServicePlugin() { return new Filespace; }
+    inline QString iconName() const { return QString("filespace.jpg"); }
+    inline QString serviceName() const { return QString("Filespace"); }
+    QRegExp urlPattern() const;
+    bool urlSupported(const QUrl &url) const;
+    void checkUrl(const QUrl &url);
+    void getDownloadRequest(const QUrl &url);
+    void login(const QString &username, const QString &password);
+    inline bool loginSupported() const { return true; }
+    inline bool recaptchaRequired() const { return true; }
+    inline QString recaptchaServiceName() const { return QString("SolveMedia"); }
+    inline QString recaptchaKey() const { return m_captchaKey; }
+    inline QString errorString() const { return m_errorString; }
+    inline int maximumConnections() const { return m_connections; }
+    bool cancelCurrentOperation();
+
+public slots:
+    void submitCaptchaResponse(const QString &challenge, const QString &response);
+
+private:
+    void getWaitTime();
+    void startWait(int msecs);
+    inline void setErrorString(const QString &errorString) { m_errorString = errorString; }
+
+private slots:
+    void checkLogin();
+    void checkUrlIsValid();
+    void onWebPageDownloaded();
+    void checkWaitTime();
+    void downloadCaptcha();
+    void onCaptchaSubmitted();
+    void updateWaitTime();
+    void onWaitFinished();
+
+signals:
+    void currentOperationCancelled();
+
+private:
+    QUrl m_url;
+    QString m_fileId;
+    QString m_fileName;
+    QString m_rand;
+    QString m_captchaKey;
+    QTimer *m_waitTimer;
+    int m_waitTime;
+    int m_connections;
+    QString m_errorString;
+};
+
+#endif // FILESPACE_H
